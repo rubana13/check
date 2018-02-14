d69177e70dc9bfd7cc4f04271e0dbedc78fd778a
rubana13 <36365491+rubana13@users.noreply.github.com>
2018-02-14 16:29:21 +0530

    Create vowel or consonant.cpp

---
+++ vowel or consonant.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+char letter[12]={'a','e','i','o','u','A','E','I','O','U'},al,i;
+cout<<"Enter any alphabet:";
+cin>>al;
+if(al==letter[i])
+{
+cout<<"It is a vowel";
+}
+else
+{
+cout<<"It is a consonant ";
+}
+
+}
