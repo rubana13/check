e018fa8fdb0a4390ef7ceb0c02174080503168d6
rubana13 <36365491+rubana13@users.noreply.github.com>
2018-02-14 16:02:19 +0530

    Create type of no.cpp

---
+++ type of no.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+int a;
+cout<<"Enter the number:";
+cin>>a;
+if(a>=1&&a<=100000)
+{
+if(a>0)
+{
+	cout<<"the number is positive";
+}
+}
+cout<<"The input will be negative or zero "
+}
+	return 0;
+}
