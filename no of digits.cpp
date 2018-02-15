48dc317632122743fd3a1770ce0bb6435ddbaac4
rubana13 <36365491+rubana13@users.noreply.github.com>
2018-02-15 11:52:21 +0530

    Create no of digits.cpp

---
+++ no of digits.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int a,count=0,i;
+cout<<"enter the number :";
+cin>>a;
+while(a!=0)
+{
+	a=a/10;
+	count++;
+}	cout<<count++;
+if(!cin)
+{
+	cout<<"invalid input";
+}
+
+}
