a443127fdf417bdf8e7f3833462f633d4edb896e
rubana13 <36365491+rubana13@users.noreply.github.com>
2018-02-15 11:41:35 +0530

    Create leap.cpp

---
+++ leap.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int yr;
+cout<<"enter the year:";
+cin>>yr;
+if(yr%4==0)
+{
+	if(yr%100==0)
+	{
+		if(yr%400==0)
+		{
+			cout<<yr<<"is a leap year";
+		}
+		else
+		{
+			cout<<yr<<"is not a leap year";
+		}
+	}
+	else
+	{
+		cout<<yr<<"is a leap year";
+	}
+}
+else
+{
+	cout<<yr<<"is not a leap year";
+}
+}
+
+
+	
