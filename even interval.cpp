b9ca6e4858a8d7e15d99278f78d9ce3340103669
rubana13 <36365491+rubana13@users.noreply.github.com>
2018-02-22 20:16:48 +0530

    Create even interval.cpp

---
+++ even interval.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,q;
+	cout<<"Enter the starting no:";
+	cin>>n;
+	cout<<"Enter the ending no:";
+	cin>>q;
+	while(n<q)
+	{
+	if(n%2==0)
+	{
+		cout<<n<<endl;
+	}
+		n=n+1;
+	}
+                
+}
