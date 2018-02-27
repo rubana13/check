af3c61d372684b9b76d71b9b4c6f0c99af4fa288
rubana13 <36365491+rubana13@users.noreply.github.com>
2018-02-27 17:48:00 +0530

    Create armstrong interval.cpp

---
+++ armstrong interval.cpp
@@ -0,0 +1,20 @@
+    #include <iostream>
+    using namespace std;
+     
+    int main() {
+    	int num,r,sum,temp;
+     
+        for(num=1;num<=500;num++){
+             temp=num;
+             sum = 0;
+     
+             while(temp!=0){
+                 r=temp%10;
+                 temp=temp/10;
+                 sum=sum+(r*r*r);
+             }
+             if(sum==num)
+                 printf("%d ",num);
+        } 
+    	return 0;
+    }
