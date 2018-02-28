ac6397ca420957274a2d219850438f4a7123abd3
rubana13 <36365491+rubana13@users.noreply.github.com>
2018-02-28 10:15:31 +0530

    Create multiples.cpp

---
+++ multiples.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i,d;
+	cout<<"Enter the value for d:";
+	cin>>d;
+	for(i = 1 ; i <= 5 ; i++) 
+	{
+		cout<<d*i<<endl;
+	}
+	return 0;
+}
