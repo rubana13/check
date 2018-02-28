6041308599b75b2161e76c9d94b66b8cc6e62df4
rubana13 <36365491+rubana13@users.noreply.github.com>
2018-02-28 10:33:42 +0530

    Create numeric or not.cpp

---
+++ numeric or not.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a[10]={1,2,3,4,5,6,7,8,9,0},d[10],i;
+	cout<<"Enter the value for d:";
+	cin>>d[];
+	for(i=0;i<=d;i++)
+	{
+	if(d!=a[i])
+	{
+		cout<<"NO";
+	}
+	else
+	{
+		cout<<"YES";
+	}
+	}
+	return 0;
+}
