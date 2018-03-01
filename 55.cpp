2be05353ba5b8250a51eb9d0088a57198b419de0
rubana13 <36365491+rubana13@users.noreply.github.com>
2018-03-01 21:30:12 +0530

    Create 55.cpp

---
+++ 55.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int x,y,z;
+    cin>>x>>y;
+    z=x*y;
+if(z%2==0)
+{
+ cout<<z<<" is Even";
+  }
+  else
+  {
+      cout<<z<<" is Odd";
+  }
+}
