6d1c00f90db90a26f5df3ab5a9fdc061e215cfdd
rubana13 <36365491+rubana13@users.noreply.github.com>
2018-03-02 10:13:04 +0530

    Create multiple of 7.cpp

---
+++ multiple of 7.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int n;
+cin>>n;
+if(n%7==0)
+{
+    cout<<n<<" is a multiple of 7";
+}
+else
+{
+ cout<<n<<" is not a multiple of 7";   
+}
+
+}
