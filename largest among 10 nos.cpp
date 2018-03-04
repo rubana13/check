aaa6a7f6197fec5aa64c84d211cfe23822ebcaa2
rubana13 <36365491+rubana13@users.noreply.github.com>
2018-03-04 18:27:34 +0530

    Create largest among 10 nos.cpp

---
+++ largest among 10 nos.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i; 
+    int arr[10];
+   
+ for(i = 0; i < 10; ++i)
+    {
+        
+       cout << "Enter Number " << i + 1 << " : "<<endl;
+
+       cin>>arr[i];
+    }
+
+for(i = 1;i < 10; ++i)
+    {
+      if(arr[0] < arr[i])
+           arr[0] = arr[i];
+    }
+    cout << "Largest element = " << arr[0];
+
+
+    return 0;
+}
