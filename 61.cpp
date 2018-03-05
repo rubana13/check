fe039688605770e5adfc674a3b5a4feb83514523
rubana13 <36365491+rubana13@users.noreply.github.com>
2018-03-05 13:36:54 +0530

    Create 61.cpp

---
+++ 61.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   string s;
+   int i,n;
+   cin>>s;
+   cout<<"enter the number of characters to display:";
+   cin>>n;
+ for(i = 0; i < n; ++i)
+    {
+        
+      cout<<s[i];
+    }
+
+
+
+    return 0;
+}
