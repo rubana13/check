38acbd4edb0df8d5eb4d13a3d6655cee66c9a543
rubana13 <36365491+rubana13@users.noreply.github.com>
2018-03-11 19:13:15 +0530

    Create 57.cpp

---
+++ 57.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int x;
+	int y;
+	cout<<"X="<<endl;
+	cout<<"Y="<<endl;
+	cin>>x>>y;
+	int z;
+	if(x,y<=100000)
+	{
+cout<<"Before swapping:"<<x<<" "<<y<<endl;
+z=x;
+x=y;
+y=z;
+	cout<<"After swapping:"<<x<<" "<<y;
+	}
+	else{
+	    cout<<"Invalid input";
+	}
+}
