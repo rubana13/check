1d3a037bd0e3591fc3a8e9bd32832d686ee71254
rubana13 <36365491+rubana13@users.noreply.github.com>
2018-03-11 20:07:18 +0530

    Update 96.cpp

--- 96.cpp
+++ 96.cpp
@@ -4,18 +4,19 @@ using namespace std;
 
 int main()
 {
-int n,flag=0,i=2;
-cin>>n;
+int m,flag=0,i=2;
+cin>>m;
       while(i<=floor(sqrt(n)))
               {
-                    if(n%i==0)
+                    if(m%i==0)
                                flag=1;
                      i++;
               }
-if(n==0 || n==1)
+if(m==0 || m==1)
       cout<<"neither prime nor composite";
       else if(flag==0)
-              cout<<n<<" is not a Composite number";
+              cout<<m<<" is not a Composite number";
 else
-          cout<<n<<" is a Composite number";
+          cout<<m<<" is a Composite number";
 }
+
