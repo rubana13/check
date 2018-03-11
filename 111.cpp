7cb289711559f6ecb14b62b3711ba303fce32864
rubana13 <36365491+rubana13@users.noreply.github.com>
2018-03-11 19:29:03 +0530

    Create 111.cpp

---
+++ 111.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int x,count=0,i;
+cout<<"enter the number :";
+cin>>x;
+while(x!=0)
+{
+	x=x/10;
+	count++;
+}	cout<<count++;
+if(!cin)
+{
+	cout<<"invalid input";
+}
+
+}
