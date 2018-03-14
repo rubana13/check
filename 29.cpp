f096a28380e53c1821b4281b8ffd1b12287f71dd
rubana13 <36365491+rubana13@users.noreply.github.com>
2018-03-14 20:11:14 +0530

    Create 29.cpp

---
+++ 29.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,j,k;
+    cout<<"Enter the time in minute:";
+    cin>>i;
+    if(i < 60){
+        cout<<"0 "<<i<<endl;
+    }else {
+        j = i/60;
+        k = i%60;
+        cout<<j<<"hrs"<<" "<<k<<"mins";
+    }
+    return 0;
+}
