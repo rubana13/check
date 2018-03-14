86396cfe2c9d0fd7f4be00aa7f41a75b34de299d
rubana13 <36365491+rubana13@users.noreply.github.com>
2018-03-14 20:34:09 +0530

    Create 33.cpp

---
+++ 33.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string str;
+    int count = 0,i;
+    cout << "Enter a sentence: ";
+    getline(cin, str);
+    for(i = 0;i <= str.length()-1;i++){
+        if(str[i] == ' '){
+            count++;
+        }
+    }
+    cout<<count;
+    return 0;
+}
