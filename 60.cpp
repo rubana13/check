7613a7ecac183d5ca0a939aa167cd51a677a34e2
rubana13 <36365491+rubana13@users.noreply.github.com>
2018-03-20 10:35:40 +0530

    Update 60.cpp

--- 60.cpp
+++ 60.cpp
@@ -5,6 +5,8 @@ int main()
     int i,a = 1,b = 1,ans;
     cout<<"Enter a number:"<<endl;
     cin>>i;
+    if(cin)
+    {
     for(int j = 1;j <= i;j++){
         if( j == 1){
             cout<<a;
@@ -16,6 +18,11 @@ int main()
             b = ans;
             cout<<" "<<ans;
         }
+    }
+    }
+    else
+    {
+        cout<<"Invalid input";
     }
     return 0;
 }
