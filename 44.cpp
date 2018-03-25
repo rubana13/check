86a6063fb82fff4804f987a97d65d4601b085b6f
rubana13 <36365491+rubana13@users.noreply.github.com>
2018-03-25 20:20:54 +0530

    Create 44.cpp

---
+++ 44.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    if(n>= 1 && n <=10){
+        cout<<"yes";
+    }else if(n<=11)
+    {
+        cout<<"no";
+    }
+    if(!cin)
+    {
+        cout<<"Invalid input";
+    }
+    return 0;
+}
