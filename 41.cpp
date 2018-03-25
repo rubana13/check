5e4bda0ea2903836bc5b1fc72d3d1c4709f5dc79
rubana13 <36365491+rubana13@users.noreply.github.com>
2018-03-25 19:52:51 +0530

    Create 41.cpp

---
+++ 41.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string s;
+    int n;
+    cout<<"Enter the string :"<<endl;
+    cin>>s;
+    cout<<"Enter the number of times:"<<endl;
+    cin>>n;
+  for(int j = 0;j < n;j++){
+        cout<<s<<endl;
+    }
+    return 0;
+}
