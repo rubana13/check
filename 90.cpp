28eb4ecf958b7feadedbe3ebc9f96a1554412550
rubana13 <36365491+rubana13@users.noreply.github.com>
2018-04-04 13:43:49 +0530

    Create 90.cpp
    finding numbers in alphanumeric string

---
+++ 90.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+char a[100];
+int i;
+cin>>a;
+for(i=0;a[i]!='\0';i++)
+{
+if((a[i]=='1')||(a[i]=='2')||(a[i]=='3')||(a[i]=='4')||(a[i]=='5')||(a[i]=='6')||(a[i]=='7')||(a[i]=='8')||(a[i]=='9')||(a[i]=='0'))
+{
+cout<<a[i];
+}
+}
+if(!cin)
+{
+cout<<"Invalid input";
+}
+return 0;
+}
