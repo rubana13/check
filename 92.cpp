aa8be7a55171bc3c13db5f2df3c82c7a305008b6
rubana13 <36365491+rubana13@users.noreply.github.com>
2018-04-06 10:26:20 +0530

    sum of an array

---
+++ 92.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n,a[n],sum=0,i;
+	cin>>n;
+	for(i=0;i<n;i++)
+{
+    cin>>a[i];
+}
+    for(i=0;i<=a[i];++i)
+    {
+        sum=sum+a[i];
+    }
+	cout<<sum;
+
+   if(!cin)
+    {
+        cout<<"Invalid input";
+    }
+   
+}
