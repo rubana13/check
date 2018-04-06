0c1103d0bfd1096d6676581f32464484b591cd17
rubana13 <36365491+rubana13@users.noreply.github.com>
2018-04-06 10:36:02 +0530

    print the Kth number in an array

---
+++ 94.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n,k,a[n],i;
+	cin>>n>>k;
+	for(i=0;i<n;i++)
+{
+    cin>>a[i];
+}
+	for(i=0;i<a[i];i++)
+{
+    if(a[i]==k)
+    {
+        cout<<a[i];
+    }
+}
+  
+   if(!cin)
+    {
+        cout<<"Invalid input";
+    }
+   
+}
