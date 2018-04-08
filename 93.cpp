b411f227ebe3f9e6e970ef57ff85ebfc4e4505c8
rubana13 <36365491+rubana13@users.noreply.github.com>
2018-04-08 17:00:14 +0530

    celsius into kelvin

---
+++ 93.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int celsius,kelvin;
+	cout<<"Enter the temperature in celsius:"<<endl;
+	cin>>celsius;
+	kelvin=celsius+273;
+    cout<<"Temperature in kelvin: " << kelvin;
+}
