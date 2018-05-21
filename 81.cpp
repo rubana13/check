898fed28b34e9fc88eb893d84aec6deedc2ee42a
rubana13 <36365491+rubana13@users.noreply.github.com>
2018-05-21 10:39:29 +0530

    kabali

---
+++ 81.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+int a,b;
+cout<<"number of ninjas in kabali and his opponent:";
+cin>>a>>b;
+cout<<"\n";
+if((a<b)&&(a,b<=2^32))
+{
+cout<<b-a;	
+}
+else
+{
+	cout<<"Invalid input";
+}
+
+}
