60aab689c1c90cace1d14f6c59e0d7857baa662f
rubana13 <36365491+rubana13@users.noreply.github.com>
2018-05-21 10:26:59 +0530

    splitting of even and odd strings

---
+++ 85.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+char a[20];
+int i;
+cin>>a[i];
+for(i=0;a[i]!=0;i=i+2)
+{
+	cout<<a[i];
+
+}
+cout<<"  ";
+for(i=0;a[i]!=0;i=i+3)
+{
+	cout<<a[i];
+}
+	return 0;
+}
