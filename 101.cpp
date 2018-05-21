6b31f0a00d060fd5885043211c98b01a450b48f2
rubana13 <36365491+rubana13@users.noreply.github.com>
2018-05-21 11:36:19 +0530

    print last characters in a string

---
+++ 101.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main() {
+char a[50];
+int n,i,l;
+cin>>a[i];
+cin>>n;
+l=strlen(a);
+for(i=l;i<=n;i++)
+{
+	cout<<a[l-1];
+l--;
+	
+}
+}
